1298d07d580bd16d726ea2839e3c90502f14e9cc
Asraful Islam Tanoy <tonoyfrombd@gmail.com>
2024-04-13 14:08:09 +0600

    first commit

---
+++ src/main/java/com/example/crood/CroodApplication.java
@@ -0,0 +1,13 @@
+package com.example.crood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CroodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CroodApplication.class, args);
+	}
+
+}
